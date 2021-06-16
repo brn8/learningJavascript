7bf7875dfcb8ad0ea9711a2fe4658874efde94ca
brn <brn8@njit.edu>
2021-06-16 01:06:47 -0400

    leetcode #1588

---
+++ Sum_of_All_Odd_Length_Subarrays.js
@@ -0,0 +1,17 @@
+var sumOddLengthSubarrays = function(arr) {
+	var count=0
+	for (var i=0; i<arr.length; i++){
+		for(var j=0; j<=arr.length; j++){
+			if(j%2){
+				if(i+j<=arr.length){
+					for(var k=i; k<j+i; k++){
+						count=count+arr[k]
+					}
+				}
+			}
+		}
+	}
+	return count
+    
+};
+console.log(sumOddLengthSubarrays([1,4,2,5,3]))
