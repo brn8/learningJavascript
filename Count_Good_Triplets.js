0205cb345ce27e526217b22b8350aa5db38a1312
brn <brn8@njit.edu>
2021-06-17 19:16:48 -0400

    leetcode #1534

---
+++ Count_Good_Triplets.js
@@ -0,0 +1,16 @@
+var countGoodTriplets = function(arr, a, b, c) {
+	var count =0;
+	for (var i=0; i<arr.length; i++){
+		for(var j=i+1; j<arr.length; j++){
+			for (var k=j+1; k<arr.length; k++){
+				if(Math.abs(arr[i]-arr[j])<=a && Math.abs(arr[j]-arr[k])<=b &&Math.abs(arr[i]-arr[k])<=c){
+					count=count+1
+				}
+			}
+		}
+		
+	}
+	return count
+    
+};
+console.log(countGoodTriplets([3,0,1,1,9,7], 7, 2, 3))
