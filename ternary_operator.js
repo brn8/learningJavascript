1798688d1bff7a240da083f8f6dac3dbd58648d8
brn <brn8@njit.edu>
2021-06-22 15:44:43 -0400

    ternary operator

---
+++ ternary_operator.js
@@ -0,0 +1,10 @@
+function ternary (weather){
+	return (weather ? "It's raining" : "It's not raining");
+}
+console.log(ternary(null))
+
+
+var age = 21;
+
+var beverage = ((age>=21)? "Beer" : "Juice");
+console.log(beverage) 
