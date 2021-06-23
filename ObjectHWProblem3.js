8bd286fd89e656826fce132f9e8ba389b5ce9c8a
brn <brn8@njit.edu>
2021-06-23 00:06:33 -0400

    add the function to the program

--- ObjectHWProblem3.js
+++ ObjectHWProblem3.js
@@ -1,12 +1,18 @@
 //identify weather given number (#13) is a prime or not 
-var num = 13;
-var arr=[];
-for (var i=1; i<=num; i++){
-	if(num%i==0){
-		arr.push(i)
+
+function checksForPrimeNumber(num){
+	var arr=[];
+	for (var i=1; i<=num; i++){
+		if(num%i==0){
+			arr.push(i)
+		}
+	}
+
+	if(arr.length==2){
+		console.log("It's a Prime number")
+	}
+	else{
+		console.log("It's not a Prime number")
 	}
 }
-
-if(arr.length==2){
-	console.log("It's a Prime number")
-}
+checksForPrimeNumber(10)
--- ObjectHWProblem4.js
+++ ObjectHWProblem4.js
@@ -1,16 +1,18 @@
 // display all the prime number from an given array 
-var num = [65,42,32,7,11,67,88,97,91,77,37,79]
-var arr= []
-for (var i=0; i<num.length; i++){
-	var count = 0
-	for (var j=1; j<=num[i]; j++ ){
-		if (num[i]%j==0){
-			count = count + 1;
+function checksForPrimeNumber(num){
+	var arr= []
+	for (var i=0; i<num.length; i++){
+		var count = 0
+		for (var j=1; j<=num[i]; j++ ){
+			if (num[i]%j==0){
+				count = count + 1;
+			}
+		}
+		if (count == 2){
+			arr.push(num[i])
 		}
 	}
-	if (count == 2){
-		arr.push(num[i])
-	}
-}
 
-console.log(arr)
+	console.log(arr)
+}
+checksForPrimeNumber([65,42,32,7,11,67,88,97,91,77,37,79])
