634b351e2028bf4f2c3002828ca3088dc3d466d7
brn <brn8@njit.edu>
2021-06-27 23:26:02 -0400

    leetcode# 1822

---
+++ Sign_of_the_Product_of_an_Array.js
@@ -0,0 +1,18 @@
+var arraySign = function(nums) {
+	var product = nums[0];
+	var value = 0
+	for (var i=1; i<nums.length; i++){
+		product=product*nums[i]
+	}
+	if(product>0){
+		return 1
+	}
+	else if (product<0){
+		return -1
+	}
+	else{
+		return 0
+	}
+    
+};
+console.log(arraySign([-1,1,1,0,-1]))
