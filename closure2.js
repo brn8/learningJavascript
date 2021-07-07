b57e0d18ab4c8de269e5d6ae3ad8305ba26da506
brn <brn8@njit.edu>
2021-07-07 03:46:58 -0400

    closure

---
+++ closure2.js
@@ -0,0 +1,35 @@
+//calculating student grade
+
+function studentGrades(name){
+	var grade = 0;
+	var average = 0;
+	var count = 0
+	function printStudentgrade(){
+		console.log("Name:"+name, "current grade:"+averageGrade())
+	}
+	function currentGrade(grades){
+		grade+=grades;
+		count++;
+
+	}
+	function averageGrade(){
+		if(count>0){
+		average=(average+grade)/count;
+		}
+		else{
+			average+=grade;
+		}
+		return average
+	}
+	return {
+		grades: currentGrade,
+		printReportCart: printStudentgrade,
+
+	}
+}
+
+var student = studentGrades("Brijesh")
+student.grades(10);
+student.grades(12);
+student.printReportCart()
+
